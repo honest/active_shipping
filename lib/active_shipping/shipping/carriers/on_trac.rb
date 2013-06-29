6f5d462950311f13ca3233ac9e0d02227d1dcf71
Brian Smith <brianthecoder@gmail.com>
2013-06-29 12:44:09 -0700

    basic on trac integration

--- lib/active_shipping/shipping/carriers/on_trac.rb
+++ lib/active_shipping/shipping/carriers/on_trac.rb
@@ -1,9 +1,9 @@
 module ActiveMerchant
   module Shipping
-    class OnTrac < Carrier      
+    class OnTrac < Carrier
       cattr_reader :name
       @@name = "On Trac"
-      
+
       TEST_URL = 'https://www.shipontrac.net/OnTracTestWebServices/OnTracServices.svc'
       LIVE_URL = 'https://www.shipontrac.net/OnTracWebServices/OnTracServices.svc'
 
@@ -17,25 +17,65 @@ module ActiveMerchant
           packages: build_packages(origin, destination, packages, options)
         })
         response = Hash.from_xml(save_request(ssl_get(url)))['OnTracRateResponse']
-        if response['Error']
-          Response.new(false, response['Error'], {}, {test: test_mode?})
+        error = response['Error'] || response['Shipments']['Shipment']['Error']
+        if error.present?
+          Response.new(false, error, {}, {test: test_mode?})
         else
           details = response['Shipments']['Shipment']['Rates']['Rate']
           Response.new(true, 'Successfully Retrieved rate', details, {test: test_mode?})
         end
       end
 
-      def create_shipment(origin, destination, packages, options = {})
-        
+      def create_shipment(origin, destination, package, options = {})
+        url = build_url("/V1/#{@options[:account]}/shipments", {
+          pw: @options[:password]
+        })
+        shipment = build_shipment(origin, destination, package, options)
+        result = save_request(ssl_post(url, shipment))
+        response = Hash.from_xml(result)['OnTracShipmentResponse']
+        error = response['Error'] || response['Shipments']['Shipment']['Error']
+        if error.present?
+          Response.new(false, error, {}, {test: test_mode?})
+        else
+          details = response['Shipments']['Shipment']
+          Response.new(true, 'Successfully created shipment', details, {test: test_mode?})
+        end
       end
 
       # get shipment
-      def find_tracking_info(tracking_number, options={})
-
-      end
-
-      def create_label(origin, destination, packages, options = {})
-        
+      # optional params
+      # logoFormat {BMP,GIF,PNG, and JPG}
+      # sigFormat {BMP,GIF,PNG, and JPG}
+      def find_tracking_info(tracking_numbers, options={})
+        type = options.delete(:type) || :track
+        url = build_url("/V1/#{@options[:account]}/shipments", {
+          pw: @options[:password],
+          requestType: type,
+          tn: tracking_numbers.join(',')
+        }.merge(options))
+        result = Hash.from_xml(save_request(ssl_get(url)))
+        response = case type.to_sym
+        when :details then
+          result['OnTracUpdateResponse']
+        when :track
+          result['OnTracTrackingResult']
+        end
+        error = response['Error'] || response['Shipments']['Shipment']['Error']
+        if error
+          Response.new(false, error, {}, {test: test_mode?})
+        else
+          details = response['Shipments']['Shipment']
+          message = case type.to_sym
+          when :details then
+            'Successfully retrieved shipment details'
+          when :track then
+            if result['OnTracTrackingResult']['Logo'].present?
+              details.merge(logo: result['OnTracTrackingResult']['Logo'])
+            end
+            'Successfully retrieved tracking info'
+          end
+          Response.new(true, message, details, {test: test_mode?})
+        end
       end
 
       def zips(last_update = nil)
@@ -43,7 +83,7 @@ module ActiveMerchant
         params.merge!(lastUpdate: last_update.strftime('%Y-%m-%d')) unless last_update.nil?
         url = build_url("/V1/#{@options[:account]}/Zips", params)
         response = Hash.from_xml(save_request(ssl_get(url)))['OnTracZipResponse']
-        if response['Error']
+        if response['Error'].present?
           Response.new(false, response['Error'], {}, {test: test_mode?})
         else
           zips = response['Zips']['Zip'].inject({}) do |hsh, zip_info|
@@ -68,12 +108,8 @@ module ActiveMerchant
       end
 
       def build_packages(origin, destination, packages, options = {})
-        imperial = ['US','LR','MM'].include?(origin.country_code(:alpha2))
         packages.map do |package|
-          dimensions = [:length, :width, :height].map do |axis|
-            units = imperial ? :inches : :cm
-            package.send(units, axis)
-          end.join('x')
+          dimensions = [:length, :width, :height].map{|axis| package.inches(axis) }.join('x')
           data = [
             SecureRandom.uuid, #unique id
             origin.postal_code, # origin postal code
@@ -82,12 +118,68 @@ module ActiveMerchant
             '0.00', # COD
             false, # Saturday Delivery
             package.value || 0, # declared value
-            imperial ? package.lbs : package, # weight
+            package.lbs, # weight
             dimensions, # dimensions
             'S', # service S – Sunrise, G – Gold, H – Palletized Freight, C – OnTrac Ground
           ]
           data.join(';')
         end.join(',')
+      end
+
+      def build_shipment(origin, destination, package, options = {})
+        xml = Builder::XmlMarkup.new
+        xml.OnTracShipmentRequest do
+          xml.Shipments do
+            xml.Shipment do
+              xml.UID(SecureRandom.uuid)
+              xml.shipper do
+                xml.Name(origin.name)
+                xml.Addr1(origin.address1)
+                # xml.Addr2(origin.address2)
+                # xml.Addr3(origin.address3)
+                xml.City(origin.city)
+                xml.State(origin.state)
+                xml.Zip(origin.zip)
+                xml.Contact
+                xml.Phone(origin.phone)
+              end
+              xml.consignee do
+                xml.Name(destination.name)
+                xml.Addr1(destination.address1)
+                xml.Addr2(destination.address2)
+                xml.Addr3(destination.address3)
+                xml.City(destination.city)
+                xml.State(destination.state)
+                xml.Zip(destination.zip)
+                xml.Contact
+                xml.Phone(destination.phone)
+              end
+              xml.Service('S')
+              xml.SignatureRequired(false)
+              xml.Residential(false)
+              xml.SaturdayDel(false)
+              xml.Declared(package.value || 0)
+              xml.COD(0)
+              xml.CODType('NONE')
+              xml.Weight(package.lbs)
+              xml.BillTo(0)
+              xml.Instructions('Ring Bell')
+              xml.Reference('Awe343')
+              xml.Reference2
+              xml.Reference3
+              xml.Tracking
+              xml.ShipEmail
+              xml.DelEmail
+              xml.DIM do
+                xml.Length(package.inches(:length))
+                xml.Width(package.inches(:width))
+                xml.Height(package.inches(:height))
+              end
+              xml.LabelType(0)
+              xml.ShipDate(Time.now.strftime('%Y-%m-%d'))
+            end
+          end
+        end
       end
     end
   end
--- test/unit/carriers/on_trac_test.rb
+++ test/unit/carriers/on_trac_test.rb
@@ -5,19 +5,26 @@ class OnTracTest < Test::Unit::TestCase
     @carrier = OnTrac.new(account: 37, password: 'testpass', test: true)
     @packages  = TestFixtures.packages
     @locations = TestFixtures.locations
-
-  end
-
-  def test_find_rates
-    beverly_hills = @locations[:beverly_hills]
-    old_honest = Location.new({
+    @beverly_hills = @locations[:beverly_hills]
+    @old_honest = Location.new({
       address1: "1550 17th Street",
       city: "Santa Monica",
       state: "CA",
       postal_code: "90404",
       country: 'US'
     })
-    resp = @carrier.find_rates(beverly_hills, old_honest, [@packages[:chocolate_stuff]])
+    @chocolate = @packages[:chocolate_stuff]
+  end
+
+  def test_invalid_rate_credentials
+    @carrier = OnTrac.new(account: 37, password: 'badpass', test: true)
+    assert_raise ActiveMerchant::Shipping::ResponseError do
+      @carrier.find_rates(@beverly_hills, @old_honest, [@chocolate])
+    end
+  end
+
+  def test_find_rates
+    resp = @carrier.find_rates(@beverly_hills, @old_honest, [@chocolate])
     assert resp.success?
     assert resp.test
     assert_equal resp.message, "Successfully Retrieved rate"
@@ -26,17 +33,72 @@ class OnTracTest < Test::Unit::TestCase
     assert_equal resp.params['TotalCharge'], "27.17"
   end
 
-  # def test_zips_success
-  #   zips = @carrier.zips
-  #   assert zips.success?
-  #   assert zips.test
-  #   assert_equal zips.message, "Successfully Retrieved zips"
-  #   assert /\d{5}/.match(zips.params.first[0])
+  def test_find_rates_error
+    @dishonest = Location.new(@old_honest.to_hash.merge(postal_code: 'ABCDE'))
+    assert_raise ActiveMerchant::Shipping::ResponseError do
+      @carrier.find_rates(@beverly_hills, @dishonest, [@chocolate])
+    end
+  end
+
+  def test_zips_success
+    zips = @carrier.zips
+    assert zips.success?
+    assert zips.test
+    assert_equal zips.message, "Successfully Retrieved zips"
+    assert /\d{5}/.match(zips.params.first[0])
+  end
+
+  def test_zips_error
+    assert_raise ActiveMerchant::Shipping::ResponseError do
+      @carrier.zips(Time.now)
+    end
+  end
+
+  def test_create_shipment
+    resp = @carrier.create_shipment(@beverly_hills, @old_honest, @chocolate)
+    assert resp.success?
+    assert resp.test
+    assert_equal resp.message, "Successfully created shipment"
+    assert_equal resp.params['ServiceChrg'], "24.1"
+    assert_equal resp.params['FuelChrg'], "3.07"
+    assert_equal resp.params['TotalChrg'], "27.17"
+    assert_equal resp.params['SortCode'], "LAX"
+    assert resp.params['Tracking'].present?
+    assert resp.params['Error'].nil?
+  end
+
+  def test_create_shipment_error
+    @dishonest = Location.new(@old_honest.to_hash.merge(postal_code: 'ABCDE'))
+    assert_raise ActiveMerchant::Shipping::ResponseError do
+      @carrier.create_shipment(@beverly_hills, @dishonest, @chocolate)
+    end
+  end
+
+  # def test_find_tracking_info
+  #   resp = @carrier.find_tracking_info(['D10010466126749'])
+  #   assert resp.success?
+  #   assert resp.test
+  #   assert_equal resp.message, "Successfully retrieved tracking info"
   # end
 
-  # def test_zips_error
-  #   assert_raise ActiveMerchant::Shipping::ResponseError do
-  #     @carrier.zips(Time.now)
-  #   end
+  # def test_find_tracking_details
+  #   resp = @carrier.find_tracking_info(['D10010466126749'], type: :details)
+  #   assert resp.success?
+  #   assert resp.test
+  #   assert_equal resp.message, "Successfully retrieved shipment details"
   # end
+
+  def test_find_tracking_info_error
+    assert_raise ActiveMerchant::Shipping::ResponseError do
+      @carrier.find_tracking_info(['ABCDE123'])
+    end
+  end
+
+  def test_create_and_track_shipment
+    resp = @carrier.create_shipment(@beverly_hills, @old_honest, @chocolate)
+    assert resp.success?
+    tracking_number = resp.params['Tracking']
+    resp = @carrier.find_tracking_info([tracking_number])
+    assert resp.success?
+  end
 end
