d3afffb989b3afd7c9f4d2c8ba234679246b0dec
Jesse Gomer <jgomer@honest.com>
2016-01-29 15:48:28 -0800

    fix typo

--- lib/active_shipping/shipping/carriers.rb
+++ lib/active_shipping/shipping/carriers.rb
@@ -9,7 +9,7 @@ require 'active_shipping/shipping/carriers/new_zealand_post'
 require 'active_shipping/shipping/carriers/canada_post_pws'
 require 'active_shipping/shipping/carriers/on_trac'
 require 'active_shipping/shipping/carriers/landmark'
-require 'active_shipping/shipping/carreiers/dhl'
+require 'active_shipping/shipping/carriers/dhl'
 module ActiveMerchant
   module Shipping
     module Carriers
